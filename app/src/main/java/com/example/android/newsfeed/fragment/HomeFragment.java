d303303be4749c786de20314382116e487d2bcef
shaddy tosh <shadrackrito@gmail.com>
2022-03-25 13:01:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/newsfeed/fragment/HomeFragment.java
@@ -0,0 +1,11 @@
+package com.example.android.newsfeed.fragment;
+
+/**
+ * The HomeFragment is a {@link BaseArticlesFragment} subclass that
+ * reuses methods of the parent class
+ */
+public class HomeFragment extends BaseArticlesFragment {
+
+    public static final String LOG_TAG = HomeFragment.class.getName();
+
+}
---
+++ app/src/main/java/com/example/android/newsfeed/fragment/ScienceFragment.java
@@ -0,0 +1,32 @@
+package com.example.android.newsfeed.fragment;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.loader.content.Loader;
+import android.util.Log;
+
+import com.example.android.newsfeed.News;
+import com.example.android.newsfeed.NewsLoader;
+import com.example.android.newsfeed.NewsPreferences;
+import com.example.android.newsfeed.R;
+
+import java.util.List;
+
+/**
+ * The ScienceFragment is a {@link BaseArticlesFragment} subclass that
+ * reuses methods of the parent class by passing the specific type of article to be fetched.
+ */
+public class ScienceFragment extends BaseArticlesFragment {
+
+    private static final String LOG_TAG = ScienceFragment.class.getName();
+
+    @NonNull
+    @Override
+    public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
+        String scienceUrl = NewsPreferences.getPreferredUrl(getContext(), getString(R.string.science));
+        Log.e(LOG_TAG, scienceUrl);
+
+        // Create a new loader for the given URL
+        return new NewsLoader(getActivity(), scienceUrl);
+    }
+}
